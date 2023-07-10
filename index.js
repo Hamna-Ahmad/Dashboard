d3fb68fb2fb8b7511be527a010d110fb391718fd
Hamna Ahmad <104259677+Hamna-Ahmad@users.noreply.github.com>
2023-07-10 12:02:12 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+// Background 
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=beach")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
@@ -11,6 +12,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
+// Dogecoin
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
         if (!res.ok) {
@@ -31,6 +33,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     })
     .catch(err => console.error(err))
 
+//  Time
 function getCurrentTime() {
     const date = new Date()
     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
@@ -38,6 +41,7 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
+// Weather
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
         .then(res => {
@@ -47,7 +51,7 @@ navigator.geolocation.getCurrentPosition(position => {
             return res.json()
         })
         .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
@@ -56,3 +60,64 @@ navigator.geolocation.getCurrentPosition(position => {
         })
         .catch(err => console.error(err))
 });
+// Notes
+// Function to save notes to local storage
+function saveNotes() {
+    const notes = document.getElementById("notes-list").innerHTML;
+    localStorage.setItem("notes", notes);
+}
+
+// Function to load notes from local storage
+function loadNotes() {
+    const savedNotes = localStorage.getItem("notes");
+    if (savedNotes) {
+        document.getElementById("notes-list").innerHTML = savedNotes;
+        addDeleteListeners();
+    }
+}
+
+// Function to add delete listeners to each note item
+function addDeleteListeners() {
+    const noteItems = document.querySelectorAll("#notes-list li");
+    noteItems.forEach(function (item) {
+        if (!item.classList.contains("listeners-added")) {
+            const deleteIcon = document.createElement("i");
+            deleteIcon.classList.add("fa", "fa-trash", "delete-icon");
+            item.appendChild(deleteIcon);
+
+            item.addEventListener("mouseenter", function () {
+                deleteIcon.style.display = "block";
+            });
+
+            item.addEventListener("mouseleave", function () {
+                deleteIcon.style.display = "none";
+            });
+
+            deleteIcon.addEventListener("click", function (event) {
+                event.stopPropagation();
+                item.remove();
+                saveNotes();
+            });
+
+            item.classList.add("listeners-added");
+        }
+    });
+}
+
+// Add event listener to save notes when the "Add Note" button is clicked
+document.getElementById("add-note-btn").addEventListener("click", function () {
+    const noteInput = document.getElementById("notes-input");
+    const noteText = noteInput.value.trim();
+
+    if (noteText !== "") {
+        const listItem = document.createElement("li");
+        listItem.textContent = noteText;
+        document.getElementById("notes-list").appendChild(listItem);
+        noteInput.value = "";
+        saveNotes();
+        addDeleteListeners();
+    }
+});
+
+// Load saved notes when the page loads
+window.addEventListener("load", loadNotes);
